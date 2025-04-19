4c2d3cd88f129e617d305ed1ac639e1df46cba03
Eduard <barned85@gmail.com>
2025-04-19 14:34:24 +0300

    add all

---
+++ myhome/materials/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Course(models.Model):
+    title_course = models.CharField(max_length=150, verbose_name='Название курса')
+    preview = models.ImageField(upload_to="materials/course/images", verbose_name='Превью', blank=True, null=True)
+    description = models.TextField(verbose_name='Описание курса')
+
+    def __str__(self):
+        return f'{self.title_course} - {self.description}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+        ordering = ["title_course"]
+
+
+class Lesson(models.Model):
+    title_lesson = models.CharField(max_length=150, verbose_name='Название урока')
+    preview = models.ImageField(upload_to="materials/lesson/images", verbose_name='Превью', blank=True, null=True)
+    description = models.TextField(verbose_name='Описание урока')
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, verbose_name='Курс', related_name='lesson', null=True, blank=True
+    )
+    link_to_video = models.URLField(verbose_name='Ссылка на видео')
+
+    def __str__(self):
+        return f'{self.title_lesson}'
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+        ordering = ['title_lesson']
---
+++ myhome/materials/views.py
@@ -0,0 +1,33 @@
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonCreateAPIView(CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveAPIVew(RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyAPIView(DestroyAPIView):
+    queryset = Lesson.objects.all()
