e7a153e9b60cbcd4dd92fb58f13911b916126295
Eduard <barned85@gmail.com>
2025-04-20 11:31:22 +0300

    fixes

--- myhome/materials/models.py
+++ myhome/materials/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Course(models.Model):
-    title_course = models.CharField(max_length=150, verbose_name='Название курса')
+    title = models.CharField(max_length=150, verbose_name='Название курса')
     preview = models.ImageField(upload_to="materials/course/images", verbose_name='Превью', blank=True, null=True)
     description = models.TextField(verbose_name='Описание курса')
 
     def __str__(self):
-        return f'{self.title_course} - {self.description}'
+        return f'{self.title} - {self.description}'
 
     class Meta:
         verbose_name = 'Курс'
@@ -20,7 +20,7 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to="materials/lesson/images", verbose_name='Превью', blank=True, null=True)
     description = models.TextField(verbose_name='Описание урока')
     course = models.ForeignKey(
-        Course, on_delete=models.CASCADE, verbose_name='Курс', related_name='lesson', null=True, blank=True
+        Course, on_delete=models.CASCADE, verbose_name='Курс', related_names='lesson', null=True, blank=True
     )
     link_to_video = models.URLField(verbose_name='Ссылка на видео')
 
--- myhome/materials/urls.py
+++ myhome/materials/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
                   path ( 'lesson/create/', LessonCreateAPIView.as_view (), name='lesson_create' ),
                   path ( 'lesson/', LessonListAPIView.as_view (), name='lesson_list' ),
                   path ( 'lesson/<int:pk>/', LessonRetrieveAPIVew.as_view (), name='lesson_detail' ),
-                  path ( 'lesson/update/<int:pk>/', LessonUpdateAPIView.as_view (), name='lesson_update' ),
+                  path ( 'lesson/<int:pk>/update/', LessonUpdateAPIView.as_view (), name='lesson_update' ),
                   path ( 'lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view (), name='lesson_delete' ),
               ] + router.urls
