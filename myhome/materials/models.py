d9f078e66809c715b81b0d8f40e4d632e528416b
Eduard <barned85@gmail.com>
2025-04-20 18:52:48 +0300

    fixes users fixes materials

--- myhome/materials/models.py
+++ myhome/materials/models.py
@@ -12,22 +12,22 @@ class Course(models.Model):
     class Meta:
         verbose_name = 'Курс'
         verbose_name_plural = 'Курсы'
-        ordering = ["title_course"]
+        ordering = ["title"]
 
 
 class Lesson(models.Model):
-    title_lesson = models.CharField(max_length=150, verbose_name='Название урока')
+    title = models.CharField(max_length=150, verbose_name='Название урока')
     preview = models.ImageField(upload_to="materials/lesson/images", verbose_name='Превью', blank=True, null=True)
     description = models.TextField(verbose_name='Описание урока')
     course = models.ForeignKey(
-        Course, on_delete=models.CASCADE, verbose_name='Курс', related_names='lesson', null=True, blank=True
+        Course, on_delete=models.CASCADE, verbose_name='Курс', related_name='lessons', null=True, blank=True
     )
     link_to_video = models.URLField(verbose_name='Ссылка на видео')
 
     def __str__(self):
-        return f'{self.title_lesson}'
+        return self.title
 
     class Meta:
         verbose_name = 'Урок'
         verbose_name_plural = 'Уроки'
-        ordering = ['title_lesson']
+        ordering = ["title"]
